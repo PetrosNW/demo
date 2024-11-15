27c8f3660174792be5360e15025426f26bbf81c3
Peter Waweru <peterngurewaweru121@gmail.com>
2024-11-15 13:11:36 +0300

    Added Python files
    guess.py and login.py

---
+++ guess.py
@@ -0,0 +1,20 @@
+
+import random
+guess = random.randint(1,10)
+no_guesses = 0
+guess_limit =3
+
+print("You have 3 guesses to win a prize")
+print("Guess a number between 1 and 10")
+
+while no_guesses < guess_limit:
+    guess_number = int(input("Guess: "))
+    no_guesses += 1
+    if guess_number == guess:
+        print("YOu won")
+        break
+else:
+    print("Game over")
+      
+     
+        
---
+++ login.py
@@ -0,0 +1,3 @@
+weight = input("Please enter your weight in kg: ");
+weight_grams = float(weight) / 1000;
+print("you are "+ str(weight_grams) + " grams")
